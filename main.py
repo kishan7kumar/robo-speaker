5c80717f5141b27004a316de8a265ddecbef0cbf
kishan7kumar <kishankumar3202@gmail.com>
2023-07-28 22:49:33 +0530

    Add final code

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+# This is just a very basic robot speaker that use computer's text to speech tool using python os package
+import os
+
+# This is the main entry point
+if __name__ == '__main__':
+    print("Welcome to Robo Speaker!")
+    while True:
+        x = input("enter the sentence to say: ")
+        if (x == 'bye'):
+            break
+        command = f"say {x}"
+        os.system(command)
